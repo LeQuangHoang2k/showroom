50cf586bc334bbdca59d23a7668cbe70c418c9c1
your username <theibdn159555@gmail.com>
2021-07-23 23:10:46 +0700

    final

---
+++ model/model.js
@@ -0,0 +1,66 @@
+exports.create = function (datasource){
+    return{
+        authenticate : function (username, password, callback){
+            datasource.loadAccount(function(account){
+                var success = account.username == username && account.password == password
+                callback(success)
+            })
+        },
+        getGeneralInfo : datasource.loadGeneralInfo,
+        editGeneralInfo : function (info, featureImageTmpPath, callback){
+            if (info.companyNameShort == '') {
+                callback('Company name short cannot be emty')
+                return
+            }
+            if (info.companyNameLong == '') {
+                callback('Company name short cannot be emty')
+                return
+            }
+            datasource.saveGeneralInfo (info, featureImageTmpPath, function (err){
+                if (err){
+                    callback('Could not save general infor')
+                    return
+                }
+                callback (false)
+            })
+        },
+
+        getAbout : datasource.loadAbout,
+
+        editAbout : function (text, callback){
+            if (text == '') {
+                    callback('About text cannot be empty')
+                    return  
+            }             
+            datasource.saveAbout (text, function(err){
+                if (err){
+                        callback('Could not save about text')
+                    }
+                    callback(false)
+                })
+                
+            },
+            getProducts : datasource.loadProducts,
+            
+            addProduct : function (editProductId, name, imageTmpPath, callback){
+                if (name == ''){
+                    callback('Product name cannot be empty')
+                    return
+                }
+                if (editProductId == 0 && imageTmpPath == ""){
+                    callback('Product image cannot be empty')
+                    return
+                }
+                datasource.addProduct(editProductId, name, imageTmpPath, function(err){
+                    if (err) {
+                        callback('Could not add product')
+                        return
+                    }
+                    callback(false)
+                })
+            },
+            getProduct : datasource.loadSingleProduct, 
+            
+            deleteProduct : datasource.deleteProduct
+    }
+}
